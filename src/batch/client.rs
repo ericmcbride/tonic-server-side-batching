d9def48e1c6c25a013870b6d3b031518cf8ab52f
Eric McBride <eric.mcbride@toyotaconnected.com>
2020-03-04 12:53:59 -0600

    end to end

--- src/batch/client.rs
+++ src/batch/client.rs
@@ -1,7 +1,7 @@
 use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
 use futures::future;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 
 pub mod hello_world {
@@ -13,17 +13,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = GreeterClient::connect("http://[::1]:50051").await?;
     let mut tasks = vec![];
 
-    for n in 1..10000 {
+    for n in 1..10001 {
         let mut client = client.clone();
         let request = tonic::Request::new(HelloRequest {
             name: format!("Tonic-{}", n),
         });
 
         tasks.push(tokio::task::spawn(async move {
-            let response = client.say_hello(request).await.unwrap();
+            let _response = client.say_hello(request).await.unwrap();
         }));
     }
+
     let now = Instant::now();
+    println!("Running tasks");
     let _ = future::join_all(tasks).await;
     println!("{}ms elapsed", now.elapsed().as_millis());
     Ok(())
--- src/batch/server.rs
+++ src/batch/server.rs
@@ -3,11 +3,12 @@ use tonic::{transport::Server, Request, Response, Status};
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
 use tokio::sync::{mpsc, oneshot};
-use tokio::time::{self, Duration};
-use tracing::{debug, error, event, info, Level};
+use tracing::{info, Level};
 use tracing_subscriber;
 use uuid::Uuid;
 use ringbuf::RingBuffer;
+use std::collections::VecDeque;
+
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -17,7 +18,7 @@ pub mod hello_world {
 /// batching, we are prepared to send back the single response
 #[derive(Debug)]
 pub struct RequestWrapper {
-    single_shot: oneshot::Sender<i32>,
+    single_shot: oneshot::Sender<String>,
     req: Request<HelloRequest>,
 }
 
@@ -36,20 +37,24 @@ impl Greeter for MyGreeter {
     #[tracing::instrument]
     async fn say_hello(
         &self,
-        request: Request<HelloRequest>,
+        mut request: Request<HelloRequest>,
     ) -> Result<Response<HelloReply>, Status> {
-        info!("request made {:?}", request);
-        let (tx, rx) = oneshot::channel::<i32>();
-        let request_wrapper = RequestWrapper {
+        let (tx, rx) = oneshot::channel::<String>();
+        let header_resp = request.metadata().get("extra-id".to_string()).unwrap().as_bytes();
+        let header_str = std::str::from_utf8(header_resp).unwrap().to_string();
+    
+        let mut request_wrapper = RequestWrapper {
             single_shot: tx,
             req: request,
         };
-
         let _ = self.inbound_sender.clone().send(request_wrapper).await;
-        //let resp = rx.await;
-        //info!("Resp is {:?}", resp);
+        let resp = rx.await.unwrap();
+        
+        // Make sure the header, and value is the same on return to client
+        assert_eq!(header_str, resp);
+
         let reply = hello_world::HelloReply {
-            message: format!("Number is 1"),
+            message: format!("Number is {}", resp),
         };
         Ok(Response::new(reply))
     }
@@ -68,7 +73,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         inbound_sender: inbound_tx,
     };
     
-    let svc = GreeterServer::with_interceptor(greeter, append_id);
+    let svc = GreeterServer::with_interceptor(greeter, header_interceptor);
 
     tokio::spawn(async move { batch_scheduler(inbound_rx).await });
     Server::builder()
@@ -85,44 +90,51 @@ async fn batch_scheduler(mut rx: mpsc::Receiver<RequestWrapper>) {
     
     let buff = RingBuffer::<RequestWrapper>::new(1000000);
     let (mut prod, mut cons) = buff.split();
-
-    // Replace with a ticker of some sort
-    let mut delay = time::delay_for(Duration::from_millis(5000));
+    
+    // TODO: Add intervals (flush times i.e. if buffers not full in 250ms make requests regardless
+    // of buffer length
     loop {
         tokio::select! {
             Some(new_req) = rx.recv() => {
                 prod.push(new_req).unwrap();
                 if cons.len() == 10 {
-                    let mut batch_vec = vec![];
+                    let mut batch_vec = VecDeque::new();
                     let collector = |val| {
-                        batch_vec.push(val);
+                        batch_vec.push_back(val);
                         true
                     };
                     let _ = cons.pop_each(collector, Some(10));
-                   
-                    // TODO: Build out batch request, and singleshot the request back to the
-                    // handler.
-                    println!("Popped off cons is {:?}", batch_vec);
-                    println!("length of consumer is 1 lol");
-                
+                    
+                    // batch request will conbsume batch_vec
+                    let results = batch_request(&batch_vec).unwrap();
+                    for result in results.into_iter() {
+                        let chan = batch_vec.pop_front().unwrap();
+                        let _ = chan.single_shot.send(result);
+                    }
                 }
-                // Here you would add to check the length of a batch, then add to it, or process it
+                
                 continue
             }
         }
     }
 }
 
-/// gRPC Interceptor used to Implement Custom id for fan out method in the batch scheduler
+/// gRPC Interceptor used to Implement Custom id for fan out method in the batch scheduler.  This
+/// can be deleted, only making sure order is preserved in this implementation
 #[tracing::instrument]
-fn append_id(mut req: Request<()>) -> Result<Request<()>, Status> {
+fn header_interceptor(mut req: Request<()>) -> Result<Request<()>, Status> {
     let id = Uuid::new_v4().to_hyphenated().to_string();
     req.metadata_mut().insert("extra-id", id.parse().unwrap());
     Ok(req)
 }
 
+/// Simulates posting a batch request, then returning a batch request
 #[tracing::instrument]
-fn batch_request() -> Result<(), Box<dyn std::error::Error>> {
-    // simulate latency and create batches with random numbers
-    Ok(())
+fn batch_request(batch: &VecDeque<RequestWrapper>) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut req_vec = vec![];
+    for r in batch.iter() {
+        let val = r.req.metadata().get("extra-id".to_string()).unwrap().as_bytes();
+        req_vec.push(std::str::from_utf8(val).unwrap().to_string());
+    }
+    Ok(req_vec)
 }
